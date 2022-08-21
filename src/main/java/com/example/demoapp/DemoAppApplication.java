04ec92b53b642f961f2e361ebe4c787e4aff0c62
Salitha Chathuranga <salithachathuranga94@gmail.com>
2022-08-21 13:21:28 +0530

    add cors configs

--- src/main/java/com/example/demoapp/DemoAppApplication.java
+++ src/main/java/com/example/demoapp/DemoAppApplication.java
@@ -2,6 +2,9 @@ package com.example.demoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DemoAppApplication {
@@ -9,5 +12,15 @@ public class DemoAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoAppApplication.class, args);
 	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+//			}
+//		};
+//	}
 
 }
